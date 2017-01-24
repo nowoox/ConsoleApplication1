549d8a9e2dae71e5aa329a3835f31a69e65c160a
nowoox <nowoox@gmail.com>
2017-01-24 11:09:05 +0400

    2nd commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,11 +8,42 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        public static string CreateFIO(string surname, string name, string otchestvo)
+        {
+            string fio = surname + " " + name + " " + otchestvo;
+            return fio;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.WriteLine("New line");
+
+            Person person1 = new Person("Ефимов", "Андрей", "Васильевич");
+            Person person2 = new Person("Пушкин", "Александр", "Сергеевич");
+
+            Console.WriteLine(person1.GetFIO());
+            Console.WriteLine(person2.GetFIO());
+
             Console.ReadLine();
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name = "";
+        public string Surname = "";
+        public string Otchestvo = "";
+
+        public Person(string surname, string name, string otchestvo)
+        {
+            Name = name;
+            Surname = surname;
+            Otchestvo = otchestvo;
+        }
+
+        public string GetFIO()
+        {
+            string fio = Surname + " " + Name + " " + Otchestvo;
+            return fio;
         }
     }
 }
