ea3ab054753bd2215d6c887ec927fe06dfa8d855
nowoox <nowoox@gmail.com>
2017-01-24 09:50:18 +0400

    new commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("New line");
             Console.ReadLine();
         }
     }
